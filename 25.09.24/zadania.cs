6fb2660e362dd154914e2f030c9cdf0ebe05eef8
ZiajaNieLubieCie2 <145669243+ZiajaNieLubieCie2@users.noreply.github.com>
2024-09-25 12:48:13 +0200

    Add files via upload

---
+++ 25.09.24/zadania.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class Elo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("zadanie 1");
+        Console.WriteLine("Witaj Swiecie!");
+        Console.WriteLine("zadanie 2");
+        string imie = Console.ReadLine();
+        Console.WriteLine(imie);
+        Console.WriteLine("zadanie 3");
+        string liczba = Console.ReadLine();
+        string liczba1 = Console.ReadLine();
+        var liczba2 = Convert.ToDouble(liczba);
+        var liczba3 = Convert.ToDouble(liczba1);
+        var liczba4 = Convert.ToInt32(liczba2);
+        var liczba5 = Convert.ToInt32(liczba3);
+        Console.WriteLine(liczba4 + liczba5);
+        Console.WriteLine("zadanie 4");
+        var r = Console.ReadLine();
+        var r1 = Convert.ToDouble(r);
+        Console.WriteLine(3.14 * (r1 * r1));
+        Console.WriteLine("zadanie 5");
+        var temp = Console.ReadLine();
+        var temp1 = Convert.ToDouble(temp);
+        Console.WriteLine((temp1 * 9/5) + 32);
+        Console.WriteLine("zadanie 6");
+        int liczba0 = int.Parse(Console.ReadLine());
+        string liczba00 = Convert.ToString(liczba0);
+        Console.WriteLine("zadanie 7");
+        double liczba7 = int.Parse(Console.ReadLine());
+        double liczba8 = int.Parse(Console.ReadLine());
+        Console.WriteLine((liczba7 + liczba8) / 2);
+        Console.WriteLine("zadanie 8");
+        var liczba99 = int.Parse(Console.ReadLine());
+        double liczba100 = Convert.ToDouble(liczba99);
+        Console.WriteLine(liczba99 * liczba99);
+        Console.WriteLine("zadanie 9");
+        double liczba44 = double.Parse(Console.ReadLine());
+        int liczba55 = Convert.ToInt32(liczba44);
+        Console.WriteLine(liczba55);
+
+
+
+
+
+    }
+        }
